887c610c5e5b19ae1bc0bbf50d5dad0695972ca9
Raghav <raghavrastogi1990@gmail.com>
2021-04-18 16:46:28 +0530

    made templates i.e pages and configured navigation links

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.home, name='home')
+    path('',views.home, name='home'),
+    path('about' ,views.about, name='about'),
+    path('services',views.services,name='services'),
+    path('contact',views.contact,name='contact')
+    
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request ,'pages/home.html')
+
+
+def about(request):
+    return render(request,'pages/about.html')
+
+def contact(request):
+    return render(request,'pages/contact.html')
+
+def services(request):
+    return render(request,'pages/services.html')
+
+
+    
